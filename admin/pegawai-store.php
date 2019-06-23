d9274ef1eca2866ce5e527cc1e3c2e31cd9ceba9
sebastianus sembara <sembara9090@gmail.com>
2019-06-23 17:35:56 +0700

    hak akses form in pegawai

--- admin/pegawai-store.php
+++ admin/pegawai-store.php
@@ -50,37 +50,78 @@ include 'header.php';
                     <input type="text" class="form-control" placeholder="Nama Belakang" name="nama_belakang" required>
                 </div>
             </div>
-            <div class="form-group"><label>Jenis Kelamin</label></div>
-            <div class="form-group">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="jk" value="L">
-                    <label class="form-check-label" for="inlineRadio1">Laki - Laki</label>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card" style="margin-bottom: 1em;">
+                        <div class="card-header">
+                            Jenis Kelamin
+                        </div>
+                        <div class="card-body">
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" id="customRadioInline1" value="L" name="jk" class="custom-control-input">
+                                <label class="custom-control-label" for="customRadioInline1">Laki - Laki</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" id="customRadioInline2" value="P" name="jk" class="custom-control-input">
+                                <label class="custom-control-label" for="customRadioInline2">Perempuan</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" placeholder="Alamat" name="alamat" rows="3"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="number" class="form-control" placeholder="No. Telepon" name="no_tlp" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Tanggal Lahir</label>
+                        <input type="date" class="form-control" placeholder="Tanggal Lahir" name="tgl_lahir" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Pendidikan" name="pendidikan" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Tanggal Rekrut</label>
+                        <input type="date" class="form-control" placeholder="Tanggal Rekrut" name="tgl_rekrut" required>
+                    </div>
                 </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="jk" value="P">
-                    <label class="form-check-label" for="inlineRadio2">Perempuan</label>
-                </div>
-            </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Hak Akses
+                        </div>
+                        <div class="card-body">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="barang" id="barang">
+                                <label class="custom-control-label" for="barang">Master Barang</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="pegawai" id="pegawai">
+                                <label class="custom-control-label" for="pegawai">Master Pegawai</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="supplier" id="supplier">
+                                <label class="custom-control-label" for="supplier">Master Supplier</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="t1" id="t1">
+                                <label class="custom-control-label" for="t1">Transaksi Pembelian</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="t2" id="t2">
+                                <label class="custom-control-label" for="t2">Transaksi Penjualan</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="l" id="l">
+                                <label class="custom-control-label" for="l">Laporan</label>
+                            </div>
+                        </div>
+                    </div>
 
-            <div class="form-group">
-                <textarea class="form-control" placeholder="Alamat" name="alamat" rows="3"></textarea>
-            </div>
-            <div class="form-group">
-                <input type="number" class="form-control" placeholder="No. Telepon" name="no_tlp" required>
-            </div>
-            <div class="form-group">
-                <input type="email" class="form-control" placeholder="Email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label>Tanggal Lahir</label>
-                <input type="date" class="form-control" placeholder="Tanggal Lahir" name="tgl_lahir" required>
-            </div>
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Pendidikan" name="pendidikan" required>
-            </div>
-            <div class="form-group">
-                <label>Tanggal Rekrut</label>
-                <input type="date" class="form-control" placeholder="Tanggal Rekrut" name="tgl_rekrut" required>
+                </div>
             </div>
             <div class="form-group">
                 <a href="barang.php"><button type="button" class="btn btn-secondary"><span class="fa fa-arrow-left"></span></button></a>
